9464d660cb09c48ab81d38c047c2e46a611ea170
Stefan Prodan <stefan.prodan@gmail.com>
2017-11-29 12:43:30 +0200

    SSL/TLS information with SAN list

---
+++ certinfo/handler.go
@@ -0,0 +1,48 @@
+package function
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net"
+	"net/url"
+	"strings"
+	"time"
+)
+
+func Handle(req []byte) string {
+	request := strings.ToLower(string(req))
+	if !strings.HasPrefix(request, "http") {
+		request = "https://" + request
+	}
+
+	u, err := url.Parse(request)
+	if err != nil {
+		return fmt.Sprintf("Error: %v", err)
+	}
+
+	address := u.Hostname() + ":443"
+	ipConn, err := net.DialTimeout("tcp", address, 5*time.Second)
+	if err != nil {
+		return fmt.Sprintf("SSL/TLS not enabed on%v\nDial error: %v", u.Hostname(), err)
+	}
+
+	defer ipConn.Close()
+	conn := tls.Client(ipConn, &tls.Config{
+		InsecureSkipVerify: true,
+		ServerName:         u.Hostname(),
+	})
+	if err = conn.Handshake(); err != nil {
+		return fmt.Sprintf("Invalid SSL/TLS for %v\nHandshake error: %v", address, err)
+	}
+
+	defer conn.Close()
+	addr := conn.RemoteAddr()
+	host, port, err := net.SplitHostPort(addr.String())
+	if err != nil {
+		return fmt.Sprintf("Error: %v", err)
+	}
+
+	cert := conn.ConnectionState().PeerCertificates[0]
+	return fmt.Sprintf("Host %v\nPort %v\nIssuer %v\nCommonName %v\nNotBefore %v\nNotAfter %v\nSANs %v\n",
+		host, port, cert.Issuer.CommonName, cert.Subject.CommonName, cert.NotBefore, cert.NotAfter, cert.DNSNames)
+}
---
+++ certinfo/handler_test.go
@@ -0,0 +1,36 @@
+package function
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestHandleReturnsCorrectResponse(t *testing.T) {
+	expected := "Google Internet Authority"
+	resp := Handle([]byte("www.google.com/about/"))
+
+	r := regexp.MustCompile("(?m:" + expected + ")")
+	if !r.MatchString(resp) {
+		t.Fatalf("\nExpected: \n%v\nGot: \n%v", expected, resp)
+	}
+}
+
+func TestHandleReturnsMultiSanResponse(t *testing.T) {
+	expected := ".stefanprodan.com"
+	resp := Handle([]byte("stefanprodan.com"))
+
+	r := regexp.MustCompile("(?m:" + expected + ")")
+	if !r.MatchString(resp) {
+		t.Fatalf("\nExpected: \n%v\nGot: \n%v", expected, resp)
+	}
+}
+
+func TestHandleReturnsErrorResponse(t *testing.T) {
+	expected := "connection refused"
+	resp := Handle([]byte("http://fscked.org"))
+
+	r := regexp.MustCompile("(?m:" + expected + ")")
+	if !r.MatchString(resp) {
+		t.Fatalf("\nExpected: \n%v\nGot: \n%v", expected, resp)
+	}
+}
