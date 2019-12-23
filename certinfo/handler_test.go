b89f1e6bd258a6ccf79bf02c684e496a4b8a78b2
stefanprodan <stefan.prodan@gmail.com>
2019-12-23 15:23:01 +0200

    Bump version to 1.0.0

--- certinfo/handler_test.go
+++ certinfo/handler_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestHandleReturnsCorrectResponse(t *testing.T) {
-	expected := "Google Internet Authority"
+	expected := "www.google.com"
 	resp := Handle([]byte("www.google.com/about/"))
 
 	r := regexp.MustCompile("(?m:" + expected + ")")
